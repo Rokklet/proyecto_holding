42deea33b2e58514749500a6857e481911833c1b
Rocco Fabro <rocco.a.fabro@gmail.com>
2024-05-23 18:03:19 -0300

    agregado de atributos

--- src/holding/Vendedor.java
+++ src/holding/Vendedor.java
@@ -2,5 +2,10 @@
 package holding;
 
 public class Vendedor {
+    private int cod; 
+    private String nombre;
+    private String dir;
+    private int fechaCap; //fecha de captacion
+    private Empresa empresa;
     
 }
