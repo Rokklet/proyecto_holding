c8c5f95dc74ab1f9b5ecfe737d7d7f0ef7ae580a
Santiago Longo <Santi@192.168.0.29>
2024-05-26 18:39:15 -0300

    Clase paises por ahora terminada.

--- src/holding/Pais.java
+++ src/holding/Pais.java
@@ -3,4 +3,14 @@ package holding;
 
 public class Pais {
     
+    private String nombre;
+    private int pbi;
+    private String capital;
+    
+    public Pais(String nombre, int pbi,String capital){
+        this.nombre = nombre;
+        this.pbi = pbi;
+        this.capital = capital;
+    }
+    
 }
