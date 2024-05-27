7cb8dd6e9053fd40b51c7e28ab3c8ea1ec2221e8
Rocco Fabro <rocco.a.fabro@gmail.com>
2024-05-27 01:56:35 -0300

    Se agrego un contructor y un par de metodos heredados

--- src/holding/Asesor.java
+++ src/holding/Asesor.java
@@ -1,7 +1,29 @@
 
 package holding;
 
+import java.util.ArrayList;
+import java.io.Serializable;
 
-public class Asesor {
+public class Asesor extends Usuario implements Serializable {
+    
+    private int codigo;
+    ArrayList<Empresa> empresas;
+    
+    public Asesor(String us, String pas){
+        setId(us);
+        setContraseña(pas);
+    }
+    
+    @Override
+    public boolean proceder(Sistema sistema){
+        EntradaSalida.mostrarString("Hola asesor " + getId());
+        return true;
+    }
+
+    @Override
+    public void mostrar() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
     
 }
