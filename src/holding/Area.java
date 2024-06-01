02308cae915de887eb7d663a937207e34117af09
Santiago Longo <Santi@192.168.0.29>
2024-06-01 19:51:58 -0300

    Le ingrese sus metodos y atributos.

--- src/holding/Area.java
+++ src/holding/Area.java
@@ -4,4 +4,19 @@ package holding;
 
 public class Area {
     
+    private String nombre;
+    
+    public Area(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void mostrar(){
+        EntradaSalida.mostrarString("" + nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    
 }
