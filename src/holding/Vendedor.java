492641cb875069ca98fa2912d5e0e0a7f0bc8cb3
Rocklet <rocco.a.fabro@gmail.com>
2024-06-04 22:15:30 -0300

    Se modifico el metodo proceder,mostrar y se agrrego un contructor

--- src/holding/Vendedor.java
+++ src/holding/Vendedor.java
@@ -11,7 +11,7 @@ public class Vendedor extends Usuario implements Serializable {
     private LocalDate fechaEntrada;
     private int cod;
     private Empresa empresas;
-    private boolean lider;
+    private Vendedor lider;
     private ArrayList<Vendedor> vendedores;
     
     public Vendedor(String u, String p) {
@@ -20,29 +20,52 @@ public class Vendedor extends Usuario implements Serializable {
         setFechaEntrada();
     }
     
+    //Contructor vendedor en base a su lider
+    public Vendedor(int cd, String us, String pas, Vendedor lid){
+        setCod(cd);
+        setId(us);
+        setContraseña(pas);
+        setFechaEntrada();
+        this.empresas = lid.getEmpresas();
+    }
+    
     @Override
     public boolean proceder(Sistema sistema){
         EntradaSalida.mostrarString("Hola vendedor " + getId());
-        int i=EntradaSalida.leerInt("Presione '1' para ver sus datos o cualquier otra tecla para salir :");
-        switch(i){
+        mostrar();
+        int i = 0;
+        do{
+            i = EntradaSalida.leerInt("Precione 2 para salir al menu o 1 para salir del sistema");
+            switch(i){
             case 1:
-                EntradaSalida.mostrarString("Su codigo es: " + cod);
-                EntradaSalida.mostrarString("Nombre: " + nombre);
-                if(lider){
-                    EntradaSalida.mostrarString("Usted tiene a su cargo a: ");
-                    for(int j=0; i<vendedores.size(); i++){
-                        EntradaSalida.mostrarString(vendedores.get(i).nombre);
-                    }
-                }
-                EntradaSalida.mostrarString("Trabaja para la empresa: " + empresas.getNombre());
-                EntradaSalida.mostrarString("Empezo el: " + fechaEntrada);
-        }
+                return false;
+            case 2:
+                return true;
+            default:
+                EntradaSalida.mostrarString("Opcion no valida");
+                break;
+            }
+        
+        }while(i!=1 && i!=2);
+        
         return true;
     }
     
     @Override
     public void mostrar(){
-        
+        EntradaSalida.mostrarString("Su codigo es: " + getCod());
+        EntradaSalida.mostrarString("Nombre: " + getNombre());
+        if(lider!=null){
+            EntradaSalida.mostrarString("Su team leader es el vendedor: " + lider.getNombre());
+        }
+        if(!vendedores.isEmpty()){
+            EntradaSalida.mostrarString("Usted tiene a su cargo a: ");
+            for(int j=0; j<vendedores.size(); j++){
+                EntradaSalida.mostrarString(vendedores.get(j).getNombre());
+            }
+        }
+        EntradaSalida.mostrarString("Trabaja para la empresa: " + empresas.getNombre());
+        EntradaSalida.mostrarString("Empezo el: " + fechaEntrada);
     }
 
     public LocalDate getFechaEntrada() {
@@ -63,6 +86,10 @@ public class Vendedor extends Usuario implements Serializable {
     
     public Empresa getEmpresas(){
         return empresas;
+    }
+    
+    public String getNombre(){
+        return nombre;
     }
     
     
