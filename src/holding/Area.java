3a5b95b495121777ed3826c8ac09f75dc98a0f64
Rocklet <rocco.a.fabro@gmail.com>
2024-06-20 17:51:00 -0300

    Se agregó el atributo descripcion Se modifico el metodo "mostrar": Ahora muestra la descripcion.

--- src/holding/Area.java
+++ src/holding/Area.java
@@ -7,18 +7,26 @@ import java.io.Serializable;
 public class Area implements Serializable {
     
     private String nombre;
+    private String descripcion;
     
-    public Area(String nombre){
+    public Area(String nombre, String des){
         this.nombre = nombre;
+        descripcion = des;
     }
     
     public void mostrar(){
+        EntradaSalida.mostrarString("___________________________________________________________________");
         EntradaSalida.mostrarString(nombre);
+        EntradaSalida.mostrarString("Descripcion: " + descripcion);
+        EntradaSalida.mostrarString("___________________________________________________________________");
+        
     }
 
     public String getNombre() {
         return nombre;
     }
     
-    
+    public String getDescripcion(){
+        return descripcion;
+    }
 }
